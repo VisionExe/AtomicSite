1c81758d17e2d69a041499437a52b68fd2495f1a
VisionExe <139134194+VisionExe@users.noreply.github.com>
2023-08-20 15:20:02 +0700

    Add files via upload

---
+++ docus-main/app.config.ts
@@ -0,0 +1,24 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Artemis Hub',
+    description: 'Official Artemis Hub Website',
+    image: 'https://cdn.discordapp.com/attachments/1078017731359690903/1134513684820021338/Proyek_Baru_28_396AAE2.png',
+    socials: {
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {
+      logo: false,
+      showLinkIcon: false,
+      exclude: [],
+      fluid: true
+    }
+  }
+})
